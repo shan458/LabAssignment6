9640de71b6f26d14c0abece03244d014b895afe7
shan458 <77813639+shan458@users.noreply.github.com>
2021-05-07 15:52:08 -0700

    finished pseudocode

--- lab6.py
+++ lab6.py
@@ -10,3 +10,24 @@ If the number is not any of the above, then the result will be "Bar"
 
 We iterate over using a loop three times and print the results to the user. As an example "Plum Cherries Melon"
 """
+
+"""
+import random
+num = generate random number 
+
+if num is greater than 15, 
+    then the result will be "Cherries"
+otherwise if num is > 10, 
+    then the result will be "Orange"
+otherwise if num is > 5, 
+    then the result will be "Plum"
+otherwise if num is > 2, 
+    then the result will be "Melon"
+otherwise if num is > 1, 
+    then the result will be "Bell"
+otherwise
+    the the result will be "Bar"
+
+loop three times
+    print the output (fuit) to the user
+"""
